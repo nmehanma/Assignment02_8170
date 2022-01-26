4a8abb877748dc5e3f088f3e33008193b8a6a567
nmehanma <nmehanma@gmail.com>
2022-01-26 14:43:13 -0500

    initial commit

---
+++ Prog8170Assign2.Tests/ConversionTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Prog8170Assign2.Tests
+{
+    class ConversionTest
+    {
+    }
+}
---
+++ Prog8170Assign2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog8170Assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
